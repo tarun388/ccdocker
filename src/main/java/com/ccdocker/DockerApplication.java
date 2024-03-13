67e50c3c08e0d7b0cedff6988a2fc82e141205b4
Tarun Verma <36099367+tarun388@users.noreply.github.com>
2024-03-13 23:33:03 +0530

    run arbitrary command

--- src/main/java/com/ccdocker/DockerApplication.java
+++ src/main/java/com/ccdocker/DockerApplication.java
@@ -1,8 +1,47 @@
 package com.ccdocker;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+@Slf4j
 public class DockerApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        log.debug("Starting ccrun");
+
+        // Top level command
+        String cmd = args[0];
+
+        if (cmd.equals("run")) {
+            try {
+                // Create ProcessBuilder
+                ProcessBuilder processBuilder = new ProcessBuilder();
+                for (int i = 1; i < args.length; i++) {
+                    processBuilder.command().add(args[i]);
+                }
+
+                // Start the process
+                Process process = processBuilder.start();
+
+                // Read output
+                BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println(line);
+                }
+
+                // Wait for the process to finish
+                int exitCode = process.waitFor();
+                log.debug("Exited with code: " + exitCode);
+
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Usage: ccrun [OPTIONS] COMMAND");
+        }
     }
 }
